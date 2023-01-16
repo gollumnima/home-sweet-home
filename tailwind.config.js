c4761b68ac0207f3f8253cd39a1ae08bfcaa8b62
gollumnima <doori.alice.kim@gmail.com>
2023-01-16 23:47:57 +0900

    [add] 초기세팅

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  extend: {},
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    themes: ['bumblebee'],
+  },
+};
